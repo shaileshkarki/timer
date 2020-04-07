bee5b5282bc1c1aa7aea28da7e7ae6088f9e0936
Shailesh <shlshkrk@gmail.com>
2020-04-07 12:45:11 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Timer");
+
+//DOM API
+//timer API - builtin library in the browser
+
+// setTimeout(function () {}, 5000);
+
+var startBtn = document.querySelector('start-btn');
+var pauseBtn = document.querySelector('pause-btn');
+var resetBtn = document.querySelector('reset-btn');
+var display = document.querySelector('display');
