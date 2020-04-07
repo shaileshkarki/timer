e0c845cfd5052098850fc0298f5e686573805c1c
Shailesh <shlshkrk@gmail.com>
2020-04-07 14:33:08 +1000

    first working version

--- app.js
+++ app.js
@@ -5,7 +5,37 @@ console.log("Timer");
 
 // setTimeout(function () {}, 5000);
 
-var startBtn = document.querySelector('start-btn');
-var pauseBtn = document.querySelector('pause-btn');
-var resetBtn = document.querySelector('reset-btn');
-var display = document.querySelector('display');
+var startBtn = document.querySelector('.start-btn');
+var pauseBtn = document.querySelector('.pause-btn');
+var resetBtn = document.querySelector('.reset-btn');
+var display = document.querySelector('.display');
+var timerId = null;
+var seconds = 0;
+
+var startTimer = function () { 
+    seconds++;
+    display.textContent  = seconds;
+}
+
+var  handleStart = function () {
+    if(timerId) {
+
+    } else {
+        timerId = setInterval(startTimer, 1000);
+    }    
+}
+var  handlePause = function () {
+    clearInterval(timerId);
+    timerId = null;
+    
+}
+var  handleReset = function () {
+    clearInterval(timerId);
+    timerId = null; 
+    seconds = 0;
+    display.textContent  = 0;
+}
+
+startBtn.addEventListener('click',handleStart);
+pauseBtn.addEventListener('click',handlePause);
+resetBtn.addEventListener('click',handleReset);
